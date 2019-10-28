3509936004799928b13d18fce31ec47b43240090
Georgii Kurtanidze <gku@ciklum.com>
2019-10-28 16:17:02 +0200

    add wrapper class

--- src/bindings.js
+++ src/bindings.js
@@ -10,6 +10,11 @@ class Binder {
         this.key = key
         return this
     }
+    /**
+     * Binds key to Class
+     * @param {Class<any>} value
+     * @returns {Binder}
+     */
     toClass(value) {
         if (!isClass(value)) {
             throw new NoNClassBindingError(value)
@@ -17,14 +22,26 @@ class Binder {
         this.bindings.setBinding(this.key, bindingTypes.CLASS, value)
         return this
     }
+    /**
+     * Binds key to Value
+     * @param {*} value
+     */
     toValue(value) {
         this.bindings.setBinding(this.key, bindingTypes.VALUE, value)
     }
+    /**
+     * Binds key to other key in bindings
+     * @param {string} srcKey
+     * @returns {Binder}
+     */
     toKey(srcKey) {
         const { type, value } = this.bindings.getBinding(srcKey)
         this.bindings.setBinding(this.key, type, value)
         return this
     }
+    /**
+     * Marks binding as Singleton
+     */
     asSingleton() {
         const { type, value } = this.bindings.getBinding(this.key)
         if (type !== bindingTypes.CLASS) {
@@ -34,6 +51,12 @@ class Binder {
     }
 }
 
+/**
+ * @typedef {Object} Binding
+ * @property {bindingTypes} type
+ * @property {*} value
+ */
+
 class Bindings {
     constructor() {
         this.bindings = new Map()
@@ -42,9 +65,15 @@ class Bindings {
     upKey(key) {
         return key.toUpperCase()
     }
+    /**
+     * Returns binding for key
+     * @param {string} key 
+     * @returns {Binding} Binding for key
+     * @throws {NoBindingError} if there is not binding for key
+     */
     getBinding(key) {
         const uKey = this.upKey(key)
-        if (this.bindings.has(uKey)) {
+        if (this.hasBinding(uKey)) {
             return this.bindings.get(uKey)
         }
         throw new NoBindingError(key)
@@ -53,6 +82,19 @@ class Bindings {
         const uKey = this.upKey(key)
         this.bindings.set(uKey, { type, value })
     }
+    /**
+     * Check if there is binding for specific key
+     * @param {string} key 
+     * @returns {Boolean}
+     */
+    hasBinding(key) {
+        const uKey = this.upKey(key)
+        return this.bindings.has(uKey)
+    }
+    /**
+     * Bind key to either Class, Value or other Key
+     * @param {string} key 
+     */
     bind(key) {
         return this.binder.setKey(key)
     }
