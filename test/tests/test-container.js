b85a5153102206ee8fefc9bef7a50afb6c168d1d
Georgii Kurtanidze <gku@ciklum.com>
2019-11-04 16:10:10 +0200

    add tests for child containers

--- test/tests/test-container.js
+++ test/tests/test-container.js
@@ -2,6 +2,7 @@ const assert = require('assert').strict
 
 const Bindings = require('../../src/bindings')
 const Container = require('../../src/container')
+const { NoContainerError } = require('../../src/binding-errors')
 
 let instanceID = 0
 const testValue = 'testValue'
@@ -46,7 +47,7 @@ class TestContainer {
             assert.deepStrictEqual(instance1, instance2)
         }
     }
-    getInstanceWithDependencies() {
+    getClassInstanceWithDependencies() {
         return () => {
             this.bindings.bind('aclass').toClass(AClass)
             this.bindings.bind('bclass').toClass(BClass)
@@ -55,6 +56,25 @@ class TestContainer {
             assert.ok(bclassInstance instanceof BClass)
             assert.ok(bclassInstance.aclass instanceof AClass)
             assert.deepStrictEqual(bclassInstance.value, testValue)
+        }
+    }
+    getInstanceFromRootContainer() {
+        return () => {
+            const child1 = new Container()
+            const child2 = new Container()
+            this.container.addContainer(child1, child2)
+            this.bindings.bind('bclass').toClass(BClass)
+            child1.bindings.bind('aclass').toClass(AClass)
+            child2.bindings.bind('value').toValue(testValue)
+            const bclassInstance = this.container.getInstance('bclass')
+            assert.ok(bclassInstance instanceof BClass)
+            assert.ok(bclassInstance.aclass instanceof AClass)
+            assert.deepStrictEqual(bclassInstance.value, testValue)
+        }
+    }
+    throwsNoContainer() {
+        return () => {
+            assert.throws(() => this.container.getInstance('nobinding'), NoContainerError)
         }
     }
 }
