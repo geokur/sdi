a3a74e87fdab6a64c70c692e900412bca3c35903
Georgii Kurtanidze <gku@ciklum.com>
2019-11-04 16:09:23 +0200

    enable Child containers

--- src/container.js
+++ src/container.js
@@ -1,29 +1,54 @@
 const Dependencies = require('./dependencies')
+const Bindings = require('./bindings')
 const bindingTypes = require('./binding-types')
+const { NoContainerError } = require('./binding-errors')
+
 
 class Container {
-    constructor(bindings) {
+    constructor(bindings = new Bindings()) {
         this.bindings = bindings
         this.singletons = new Map()
         this.dependencies = new Dependencies(this)
+        this.containers = new Set()
     }
-    getClassInstance(Class) {
+    addContainer(...containers) {
+        for (let container of containers) {
+            this.containers.add(container)
+        }
+    }
+    getInstance(key) {
+        if (this.bindings.hasBinding(key)) {
+            return this._getInstance(key)
+        } else {
+            const container = this._getContainer(key)
+            return container.getInstance(key)
+        }
+    }
+    _getContainer(key) {
+        for (let container of this.containers.values()) {
+            if (container.bindings.hasBinding(key)) {
+                return container
+            }
+        }
+        throw new NoContainerError(key)
+    }
+    _getClassInstance(Class) {
         return new Class(this.dependencies)
     }
-    getSingleton(key, Class) {
+    _getSingleton(key, Class) {
         if (!this.singletons.has(key)) {
-            const instance = this.getClassInstance(Class)
+            const instance = this._getClassInstance(Class)
             this.singletons.set(key, instance)
         }
         return this.singletons.get(key)
     }
-    getInstance(key) {
+    _getInstance(key) {
         const { type, value } = this.bindings.getBinding(key)
         switch (type) {
             case bindingTypes.CLASS:
-                return  this.getClassInstance(value)
+                return  this._getClassInstance(value)
             case bindingTypes.SINGLETON:
-                return this.getSingleton(key, value)
+                return this._getSingleton(key, value)
             case bindingTypes.VALUE:
                 return value
         }
