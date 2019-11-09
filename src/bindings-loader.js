73f8be628e90f93734881f9fd9c24c4f26d1db78
Georgii Kurtanidze <gku@ciklum.com>
2019-11-09 23:15:34 +0200

    convert loadDir to static

--- src/bindings-loader.js
+++ src/bindings-loader.js
@@ -2,12 +2,10 @@ const isClass = require('./is-class')
 const Loader = require('./loader')
 
 class BindingsLoader {
-	constructor() {
-		this.loader = new Loader()
-	}
-	loadDir(dir, bindings, recursive = true) {
-		this.loader.loadDir(dir, recursive)
-		this.loader.index.forEach((value, key) => isClass(value) ? bindings.bind(key).toClass(value) : 
+	static loadDir(dir, bindings, recursive = true) {
+		const loader = new Loader()
+		loader.loadDir(dir, recursive)
+		loader.index.forEach((value, key) => isClass(value) ? bindings.bind(key).toClass(value) : 
 																	bindings.bind(key).toValue(value))
 	}
 }
